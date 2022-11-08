52f168af2f6c8da0fa0484a0a0ad255e4ad45990
Akakatsu34 <greenpigeon0304@gmail.com>
2022-11-08 21:30:17 +0900

    Revert "Merge pull request #13 from Akakatsu34/ユーザー管理機能"
    This reverts commit dad838866f469330db1171ecfa49b57adb089ee5, reversing
    changes made to f1d856796c0af835a77613652726eb3de5dba380.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,7 @@
 class ItemsController < ApplicationController
-  # def index
-  # end
 
-  # def new
-  # end
-
-  # def edit
-  # end
-
-  # def show
-  # end
+  def index
+    
+  end
 end
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,2 @@
 class Item < ApplicationRecord
-  validates :name,                 presence: true
-  validates :describe,             presence: true
-  validates :category_id,          presence: true
-  validates :condition_id,         presence: true
-  validates :shipping_charge_id,   presence: true
-  validates :ship_from_id,         presence: true
-  validates :delivery_duration_id, presence: true
-  validates :price,                presence: true
-  validates :user,                 presence: true
 end
