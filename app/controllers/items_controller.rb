09d1863b90a4d8940c68652d577fb5948e5cfb3e
Akakatsu34 <greenpigeon0304@gmail.com>
2022-11-09 17:32:48 +0900

    商品出品機能（フォーム情報をデータベースに保存）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,36 @@
 class ItemsController < ApplicationController
-  # def index
-  # end
 
-  # def new
-  # end
 
-  # def edit
-  # end
+  def index
+    @item = Item.order("created_at DESC")
+  end
 
-  # def show
-  # end
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+      params.require(:item).permit(
+        :name, 
+        :describe, 
+        :category_id, 
+        :condition_id,
+        :shipping_charge_id,
+        :ship_from_id,
+        :delivery_duration_id,
+        :price,
+        :image
+      ).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,21 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :ship_from
+  belongs_to :delivery_duration
+  has_one_attached :image
+
+
   validates :name,                 presence: true
   validates :describe,             presence: true
-  validates :category_id,          presence: true
-  validates :condition_id,         presence: true
-  validates :shipping_charge_id,   presence: true
-  validates :ship_from_id,         presence: true
-  validates :delivery_duration_id, presence: true
+  validates :category_id,          presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id,         presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_charge_id,   presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :ship_from_id,         presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_duration_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price,                presence: true
   validates :user,                 presence: true
 end
